865afd317aae004118049ced5234a3c646f527de
NithyaAppannagaari <70969819+NithyaAppannagaari@users.noreply.github.com>
2022-06-17 10:36:10 -0700

    updated main and goal page

--- App.js
+++ App.js
@@ -1,39 +1,87 @@
 import * as React from 'react';
-import {Button, View, Text} from 'react-native';
+import { Button, View, Text, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 function HomeScreen({navigation}) {
+  const staticImg = require("./assets/fonts/earth.png");
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'green' }}>
-      <Text>Be Aware</Text>
+    <View style={{ flex:1, justifyContent: 'space-evenly', alignItems: 'center', backgroundColor: '#b4cebd' }}>
+      <Text style = {{color: "#083316", fontSize: 50, fontFamily: 'blow', fontStyle: 'normal'}}>Bee Aware</Text>
       <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Info')}
+        color="#083316"
+        title="Start"
+        onPress={() => navigation.navigate('Main')}
       />
-      <Button 
-        title="Go to calculator"
-        onPress={() => navigation.navigate('Calculator')}
-      />
+      <Image 
+        source = {staticImg}
+        style={{width: 250, height: 250, position: 'absolute', right: 0, bottom: 0}}
+       />
     </View>
   );
 }
 
-function InfoScreen({navigation}) {
+function Calculator({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Info Screen</Text>
-      <Button title="Done" onPress={() => navigation.navigate('Main')} />
+    <View style={{flex:1, justifyContent: 'space-evenly', alignItems: 'center', backgroundColor: '#b4cebd'}}>
+      <Text style = {{color: "#083316", fontSize: 50, fontFamily: 'blow'}}>Calculate Your Carbon Footprint</Text>
+      <Button
+        color="#083316"
+        title="Main Page"
+        onPress={() => navigation.navigate('Main')}
+      />
     </View>
   );
 }
 
 function MainScreen({navigation}) {
+
+  const goals = ["Have a fully vegetarian lunch today", "Walk back home from your school or office today",
+    "Replace all beef you eat today with chicken", "Turn down the heating by 1 degree today", 
+    "Shower under 7 minutes today", "Turn off the water when you brush your teeth today", 
+    "Turn off the water while cleaning the dishes today", "Do not charge your phone today if it is above 50%",
+    "Use a fan instead of air conditioning in your car today", "Carpool to school or office today", 
+    "Make sure to not waste any of the food you cook today"];
+  
   return (
-    <View style={{flex:1, justifyContent: 'center', backgroundColor: '#b4cebd'}}>
-      <Text style = {{flex: 1, alignItems: 'center', justifyContent: 'flex-start',color: '#b4cebd', fontSize: 75, backgroundColor: '#083316'}}>Your Daily Goal Is: </Text>
-      <Text style = {{flex: 1, justifyContent: 'space-around', color: 'white', fontSize: 50, backgroundColor: '#083316'}}>Eat a purely vegetarian meal for lunch today.</Text>
+    <View style={{flex:1, justifyContent: 'space-evenly', alignItems: 'center', backgroundColor: '#b4cebd'}}>
+      <Text style = {{color: '#083316', fontSize: 75, fontFamily: 'blow'}}>Today's Goal: </Text>
+      <Text style = {{color: 'white', fontSize: 50 }}>{goals[3]}</Text>
+      <Button
+        color="#083316"
+        title="SHUFFLE"
+        onPress={() => navigation.navigate('Main')}
+      />
+
+      <Button 
+        color="#083316"
+        title="Submit a Personal Goal of Yours"
+        onPress={() => navigation.navigate('Goal')}
+      />
+
+      <Button
+        color="#083316"
+        title="Calculate Your Carbon Footprint"
+        onPress={() => navigation.navigate('Calculator')}
+      />
+
+      <Button
+        color="#083316"
+        title="Home"
+        onPress={() => navigation.navigate('Bee Aware')}
+      />
+      
     </View>
   );
+  
+}
+
+function SubmitPersonalScreen({navigation}) {
+  return(
+    <View style={{flex:1, justifyContent: 'space-evenly', alignItems: 'center', backgroundColor: '#b4cebd'}}>
+        <Text style = {{color: '#083316', fontSize: 75, fontFamily: 'blow'}}>Submit Your Personal Goal: </Text>
+    </View>
+  )
 }
 
 const Stack = createNativeStackNavigator();
@@ -43,11 +91,13 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Bee Aware">
         <Stack.Screen name="Bee Aware" component={HomeScreen} />
-        <Stack.Screen name="Info" component={InfoScreen} />
         <Stack.Screen name="Main" component = {MainScreen} />
+        <Stack.Screen name="Calculator" component = {Calculator} />
+        <Stack.Screen name = "Goal" component = {SubmitPersonalScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
+
