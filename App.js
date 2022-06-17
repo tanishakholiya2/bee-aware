abc93e84c1d4fb873e7e70da338fd9a8a9fcbba2
tanishakholiya2 <tanishakholiya@gmail.com>
2022-06-17 14:11:59 -0700

    calculator!!

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import * as React from 'react';
 import {Button, View, Text} from 'react-native';
+import Calculator from './calculator.js';
+import CarbonFootprint from './carbonFootprintInfo.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'green' }}>
-      <Text>Be Aware</Text>
+      <Text>Bee Aware</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Info')}
-      />
-      <Button 
-        title="Go to calculator"
-        onPress={() => navigation.navigate('Calculator')}
       />
     </View>
   );
@@ -28,12 +27,53 @@ function InfoScreen({navigation}) {
 }
 
 function MainScreen({navigation}) {
+
+  const goals = ["Have a fully vegetarian lunch today", "Walk back home from your school or office today",
+    "Replace all beef you eat today with chicken", "Turn down the heating by 1 degree today", 
+    "Shower under 7 minutes today", "Turn off the water when you brush your teeth today", 
+    "Turn off the water while cleaning the dishes today", "Do not charge your phone today if it is above 50%",
+    "Use a fan instead of air conditioning in your car today", "Carpool to school or office today", 
+    "Make sure to not waste any of the food you cook today"];
+  
   return (
-    <View style={{flex:1, justifyContent: 'center', backgroundColor: '#b4cebd'}}>
-      <Text style = {{flex: 1, alignItems: 'center', justifyContent: 'flex-start',color: '#b4cebd', fontSize: 75, backgroundColor: '#083316'}}>Your Daily Goal Is: </Text>
-      <Text style = {{flex: 1, justifyContent: 'space-around', color: 'white', fontSize: 50, backgroundColor: '#083316'}}>Eat a purely vegetarian meal for lunch today.</Text>
+    <View style={{flex:1, justifyContent: 'space-evenly', alignItems: 'center', backgroundColor: '#b4cebd'}}>
+      <Text style = {{color: '#083316', fontSize: 75, fontFamily: 'blow'}}>Today's Goal: </Text>
+      <Text style = {{color: 'white', fontSize: 50 }}>{goals[3]}</Text>
+      <Button
+        color="#083316"
+        title="SHUFFLE"
+        onPress={() => navigation.navigate('Main')}
+      />
+
+      <Button 
+        color="#083316"
+        title="Submit a Personal Goal of Yours"
+        onPress={() => navigation.navigate('Goal')}
+      />
+
+      <Button
+        color="#083316"
+        title="Calculate Your Carbon Footprint"
+        onPress={() => navigation.navigate('Calculator')}
+      />
+
+      <Button
+        color="#083316"
+        title="Home"
+        onPress={() => navigation.navigate('Bee Aware')}
+      />
+      
     </View>
   );
+  
+}
+
+function SubmitPersonalScreen({navigation}) {
+  return(
+    <View style={{flex:1, justifyContent: 'space-evenly', alignItems: 'center', backgroundColor: '#b4cebd'}}>
+        <Text style = {{color: '#083316', fontSize: 75, fontFamily: 'blow'}}>Submit Your Personal Goal: </Text>
+    </View>
+  )
 }
 
 const Stack = createNativeStackNavigator();
@@ -45,6 +85,9 @@ function App() {
         <Stack.Screen name="Bee Aware" component={HomeScreen} />
         <Stack.Screen name="Info" component={InfoScreen} />
         <Stack.Screen name="Main" component = {MainScreen} />
+        <Stack.Screen name="Calculator" component = {Calculator} />
+        <Stack.Screen name = "Goal" component = {SubmitPersonalScreen} />
+        <Stack.Screen name="CarbonFootprint" component = {CarbonFootprint} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ carbonFootprintInfo.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Stylesheet, Text, View, TextInput, ViewStyle, 
+TextStyle, TextInputProps, Button} from 'react-native';
+import { useState } from "react";
+
+export default function CarbonFootprint({route, navigation}) {
+    const {electricBill, gasBill, oilBill, mileage, flightsBelow, flightsAbove, recycleNewspaper, recycleAluminum, meat, people} = route.params;
+    var carbonFootprint = electricBill*105 + gasBill*105 + oilBill*113 + mileage*0.79 + flightsBelow*1100 + flightsAbove*4400;
+    if(recycleNewspaper===2) {
+        carbonFootprint = carbonFootprint + 184;
+    }
+    if(recycleAluminum===2) {
+        carbonFootprint = carbonFootprint + 166;
+    }
+    if(meat===4){
+        carbonFootprint = carbonFootprint + 200;
+    }
+    else if(meat===3){
+        carbonFootprint = carbonFootprint + 150;
+    }
+    else if(meat===2){
+        carbonFootprint = carbonFootprint + 75;
+    }
+    carbonFootprint = carbonFootprint / people;
+
+    var text = 'Your carbon footprint is ';
+    if(carbonFootprint < 8500) {
+        text = text + 'very low! Congratulations'
+    }
+    else if(carbonFootprint < 18000) {
+        text = text + 'ideal! Amazing'
+    }
+    else if(carbonFootprint < 30000) {
+        text = text + 'average. Try to take action to reduce your carbon footprint!'
+    }
+    else {
+        text = text + 'very high. Try to take action to reduce your carbon footprint!'
+    }
+    return(
+        <View>
+        <Text style={{textAlign: 'center'}}>your carbon footprint is {carbonFootprint}</Text> 
+        <Text style={{textAlign: 'center'}}>{text}</Text>
+        </View>
+    ) 
+}
