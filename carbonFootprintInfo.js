4f23136c9d472df717c1bf3168b0c994cffd80a1
NithyaAppannagaari <70969819+NithyaAppannagaari@users.noreply.github.com>
2022-06-19 09:15:24 -0700

    update carbon footprint

--- carbonFootprintInfo.js
+++ carbonFootprintInfo.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Stylesheet, Text, View, TextInput, ViewStyle, 
-TextStyle, TextInputProps, Button} from 'react-native';
+TextStyle, TextInputProps, Button, Image} from 'react-native';
 import { useState } from "react";
 
 export default function CarbonFootprint({route, navigation}) {
     const {electricBill, gasBill, oilBill, mileage, flightsBelow, flightsAbove, recycleNewspaper, recycleAluminum, meat, people} = route.params;
+    const staticImg = require("./assets/fonts/earth.png");
+    const low = require("./assets/fonts/lowFootprint.png");
+    const ideal = require("./assets/fonts/idealFootprint.png");
+    const average = require("./assets/fonts/averageFootprint.png");
+    const high = require("./assets/fonts/highFootprint.png");
     var carbonFootprint = electricBill*105 + gasBill*105 + oilBill*113 + mileage*0.79 + flightsBelow*1100 + flightsAbove*4400;
     if(recycleNewspaper===2) {
         carbonFootprint = carbonFootprint + 184;
@@ -24,27 +29,67 @@ export default function CarbonFootprint({route, navigation}) {
     carbonFootprint = carbonFootprint / people;
 
     var text = 'Your carbon footprint is ';
+    var img;
     if(carbonFootprint < 8500) {
-        text = text + 'very low! Congratulations'
+        text = text + 'very low! Congratulations!'
+        img = low;
     }
     else if(carbonFootprint < 18000) {
-        text = text + 'ideal! Amazing'
+        text = text + 'ideal! Amazing!'
+        img = ideal;
     }
     else if(carbonFootprint < 30000) {
         text = text + 'average. Try to take action to reduce your carbon footprint!'
+        img = average;
     }
     else {
         text = text + 'very high. Try to take action to reduce your carbon footprint!'
+        img = high;
     }
+    carbonFootprint = Math.round(carbonFootprint)
+
     return(
-        <View style={{flex:1, justifyContent: 'space-evenly', alignItems: 'center', backgroundColor: '#b4cebd'}}>
-        <Text style={{textAlign: 'center', fontSize: 30}}>Your carbon footprint is {carbonFootprint}</Text> 
-        <Text style={{textAlign: 'center', fontSize: 30}}>{text}</Text>
-        <Button 
-            color="#083316"
-            title = "Back to Main Page"
-            onPress = {() => navigation.navigate('Main')} 
-        /> 
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#b4cebd'}}>
+        <Image
+            style ={{width: "30%", height:"30%", position: 'absolute', bottom: -20, left: -90}}
+            source={{ uri : "https://i.pinimg.com/originals/9b/d8/1b/9bd81b44964934c34b99a0548c1b67c2.gif"}}
+          />
+      <Image
+            style ={{width: "30%", height:"30%", position: 'absolute', bottom: -20, left: 180}}
+            source={{ uri : "https://i.pinimg.com/originals/9b/d8/1b/9bd81b44964934c34b99a0548c1b67c2.gif"}}
+          />
+      <Image
+            style ={{width: "30%", height:"30%", position: 'absolute', bottom: -20, left: 380}}
+            source={{ uri : "https://i.pinimg.com/originals/9b/d8/1b/9bd81b44964934c34b99a0548c1b67c2.gif"}}
+          />
+      <Image
+            style ={{width: "30%", height:"30%", position: 'absolute', bottom: -20, left: 580}}
+            source={{ uri : "https://i.pinimg.com/originals/9b/d8/1b/9bd81b44964934c34b99a0548c1b67c2.gif"}}
+          />
+      <Image
+            style ={{width: "30%", height:"30%", position: 'absolute', bottom: -20, left: 780}}
+            source={{ uri : "https://i.pinimg.com/originals/9b/d8/1b/9bd81b44964934c34b99a0548c1b67c2.gif"}}
+          />
+      <Image
+            style ={{width: "30%", height:"30%", position: 'absolute', bottom: -20, left: 980}}
+            source={{ uri : "https://i.pinimg.com/originals/9b/d8/1b/9bd81b44964934c34b99a0548c1b67c2.gif"}}
+          />
+      <Image
+            style ={{width: "30%", height:"30%", position: 'absolute', bottom: -20, left: 1180}}
+            source={{ uri : "https://i.pinimg.com/originals/9b/d8/1b/9bd81b44964934c34b99a0548c1b67c2.gif"}}
+        />
+
+
+        <Text style= {{color: '#083316', fontSize: 30, textAlign: "center", marginLeft: 50, marginRight: 50}}>Your carbon footprint is {carbonFootprint}</Text> 
+        <Text style= {{color: '#083316', fontSize: 30, textAlign: "center", marginLeft: 50, marginRight: 50}}>{text}</Text>
+        <Image 
+        source = {img}
+        style={{width:300, height:250}}
+       />
+        <Image 
+        source = {staticImg}
+        style={{width: 250, height: 250, position: 'absolute', right: 0, bottom: 0}}
+       />
         </View>
     ) 
 }
